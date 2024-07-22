1239d0b217d2bf33bcaf8f8f79ec58c997c2efa4
AlexeyPanichev <159615024+AlexeyPanichev@users.noreply.github.com>
2024-07-22 13:39:16 +0300

    Stub and tests for findByNumber

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -2,6 +2,7 @@ import org.example.PhoneBook;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class PhoneBookTest {
     @Test
@@ -9,5 +10,20 @@ public class PhoneBookTest {
         PhoneBook phoneBook = new PhoneBook();
         int count = phoneBook.add("Boba", "123456");
         assert count == 1;
+    }
+
+    @Test
+    public void testFindByNumber() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Boba", "123456");
+        String name = phoneBook.findByNumber("123456");
+        assertEquals("Boba", name);
+    }
+
+    @Test
+    public void testFindByNumberNotFound() {
+        PhoneBook phoneBook = new PhoneBook();
+        String name = phoneBook.findByNumber("123456");
+        assertNull(name);
     }
 }
